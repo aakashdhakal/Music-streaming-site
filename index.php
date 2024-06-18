d41c1c521a8ff5b8cff20f3ec680e19349bc3ed7
Aakash Dhakal <anamoldhakal22@gmail.com>
2024-06-18 19:09:58 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta title="Name" content="Some cool slogan">
+    <title>Name - Some cool Slogan</title>
+    <link rel="shortcut icon" href="public/images/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="./public/CSS/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <?php include "./templetes/navbar.php"; ?>
+
+</body>
+
+</html>
---
+++ views/navbar.php
@@ -0,0 +1,14 @@
+<header>
+    <div class="logo-container">
+        <img src="public/images/logo.png" alt="logo">
+        <h1>Name</h1>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+</header>
